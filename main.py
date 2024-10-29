b029ae9c35fc6fa25e12ab47407d6aa43cdd2d98
matthew-sutanto <matthew.sutanto1@gdplabs.id>
2024-10-29 11:03:00 +0700

    feat(chat-ui): added FastAPI endpoint and simple html to show the UI

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama.chat_models import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+
+from fastapi import Body, FastAPI
+from fastapi.responses import FileResponse, StreamingResponse
+from pydantic import BaseModel
+
+app = FastAPI()
 
 chat_model = ChatOllama(model="llama3.2:1b")
 
 messages = [
-    SystemMessage("You are a very angry person. You don't like being in a conversation and will always want to make the other person stop talking.")
+    SystemMessage("""
+    You are a very polite and helpful assistant.
+    You will always end any response with an emoji.
+    """)
 ]
 
-while (True):
-    inp = input("You: ")
-    messages.append(HumanMessage(content=inp))
-    response = chat_model.invoke(messages)
-    print(f"Bot: {response.content}")
-    messages.append(AIMessage(content=response.content))
-    print("Messages", messages)
+@app.get("/")
+async def index():
+    return FileResponse("views/index.html")
+
+class ChatRequest(BaseModel):
+    user_message: str
+@app.post("/chat")
+async def chat_endpoint(request: ChatRequest):
+    global messages
+    async def generate():
+        messages.append(HumanMessage(content=request.user_message))
+        async for chunk in chat_model.astream(messages):
+            yield f"data: {chunk.content}\n\n"
+        messages.append(AIMessage(content=chunk.content))
+
+    return StreamingResponse(
+        generate(),
+        media_type="text/event-stream"
+    )
+
