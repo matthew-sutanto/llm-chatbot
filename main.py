9e3ac2b94e7b753de4c441f383ca8c547e8e8768
matthew-sutanto <matthew.sutanto1@gdplabs.id>
2024-10-29 17:09:09 +0700

    fix(chat-ui): fixed streaming response handling in ui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from typing import List
 from langchain_ollama.chat_models import ChatOllama
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, MessageLikeRepresentation
 
 from fastapi import Body, FastAPI
 from fastapi.responses import FileResponse, StreamingResponse
@@ -22,17 +23,19 @@ async def index():
 
 class ChatRequest(BaseModel):
     user_message: str
+
+async def generate(user_msg: str, messages: List[MessageLikeRepresentation]):
+    messages.append(HumanMessage(content=user_msg))
+    async for chunk in chat_model.astream(messages):
+        yield f"data: {chunk.content}\n\n"
+    messages.append(AIMessage(content=chunk.content))
+
 @app.post("/chat")
 async def chat_endpoint(request: ChatRequest):
     global messages
-    async def generate():
-        messages.append(HumanMessage(content=request.user_message))
-        async for chunk in chat_model.astream(messages):
-            yield f"data: {chunk.content}\n\n"
-        messages.append(AIMessage(content=chunk.content))
 
     return StreamingResponse(
-        generate(),
+        generate(request.user_message, messages),
         media_type="text/event-stream"
     )
 
